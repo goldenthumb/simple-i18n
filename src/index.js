3efe691b7fbe628950ad57103c501e441d43d6b9
yhchoi <cyh8914@gmail.com>
2018-09-05 14:18:34 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const hasOwn = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
+
+class SimpleI18n {
+  constructor({ defaultLocale = ['en'], locale = 'en', messages }) {
+    this._defaultLocale = defaultLocale;
+    this._locale = locale;
+    this._messages = { ...messages };
+  }
+
+  switchLang(locale) {
+    this._locale = locale;
+    return this;
+  }
+
+  message(key) {
+    const fallbacks = [this._locale, ...this._defaultLocale];
+
+    for (const fallback of fallbacks) {
+      if (hasOwn(this._messages[fallback], key)){
+        return this._messages[fallback][key]
+      }
+    }
+
+    return key;
+  }
+
+  messages() {
+    return { ...this._messages[this._locale] };
+  }
+
+  all() {
+    return { ...this._messages };
+  }
+}
+
+module.exports = SimpleI18n;
---
+++ src/index.test.js
@@ -0,0 +1,30 @@
+import SimpleI18n from './index';
+
+test('basic', () => {
+  const i18n = new SimpleI18n({
+    defaultLocale: ['en'],
+    locale: 'en',
+    messages: {
+      ['ko']: {
+        hello: '안녕하세요.'
+      },
+      ['en']: {
+        hello: 'hello.',
+        bye: 'goodbye.'
+      },
+      ['ja']: {
+        hello: 'こんにちは。',
+        bye: 'さようなら。'
+      }
+    }
+  });
+
+  expect(i18n.message('hello')).toBe('hello.');
+
+  i18n.switchLang('ja');
+  expect(i18n.message('bye')).toBe('さようなら。');
+
+  i18n.switchLang('ko');
+  expect(i18n.message('hello')).toBe('안녕하세요.');
+  expect(i18n.message('bye')).toBe('goodbye.');
+});
